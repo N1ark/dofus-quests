0f565004f0cc8c8efdcc3fef105caaba457b32e4
N1ark <n1ark.officiel@gmail.com>
2024-10-31 16:42:25 +0000

    Sigma

--- src/lib/components/GraphView.svelte
+++ src/lib/components/GraphView.svelte
@@ -1,55 +1,118 @@
 <script lang="ts">
+    type Position = { x: number; y: number }
+    type NodeProps = Omit<Data['nodes'][number], 'type'> & {
+        type: any
+        label: string
+        color: string
+        _type: Data['nodes'][number]['type']
+    } & Position
+    type EdgeProps = Omit<Data['edges'][number], 'type'> & {
+        type: any
+        color: string
+        _type: Data['edges'][number]['type']
+    }
+
     import Graph from 'graphology'
     import Sigma from 'sigma'
+    import ELK from 'elkjs'
+    import type { ElkNode } from 'elkjs'
     import { onMount } from 'svelte'
+    import { nodeColors, edgeColors } from '../data'
     import type { Data } from '../data'
 
     export let data: Data
 
+    const graph = new Graph<NodeProps, EdgeProps>()
+    data.nodes.forEach((node) =>
+        graph.addNode(node.id, {
+            ...node,
+            label: node.name,
+            _type: node.type,
+            type: undefined,
+            color: nodeColors[node.type],
+            x: 0,
+            y: 0,
+        })
+    )
+    data.edges.forEach((edge) =>
+        graph.addEdge(edge.from, edge.to, {
+            ...edge,
+            _type: edge.type,
+            type: 'arrow',
+            color: edgeColors[edge.type],
+        })
+    )
+
+    const doLayout = (
+        nodes: { id: string; name: string }[],
+        edges: { from: string; to: string }[]
+    ) => {
+        const elk = new ELK()
+
+        const elkGraph: ElkNode = {
+            id: 'root',
+            children: [
+                {
+                    id: 'graph',
+                    layoutOptions: {
+                        'elk.algorithm': 'layered',
+                        'elk.direction': 'UP',
+                    },
+                    children: nodes.map(({ id, name }) => ({
+                        id,
+                        width: 15,
+                        height: 1,
+                        labels: [{ text: name }],
+                        properties: {
+                            'elk.nodeLabels.placement':
+                                'OUTSIDE V_CENTER H_RIGHT',
+                        },
+                    })),
+                    edges: edges.map((edge) => ({
+                        id: `${edge.from}-${edge.to}`,
+                        sources: [edge.from],
+                        targets: [edge.to],
+                    })),
+                },
+            ],
+        }
+
+        elk.layout(elkGraph)
+            .then((elkGraph) => {
+                elkGraph.children![0].children!.forEach((node) => {
+                    const n = graph.getNodeAttributes(node.id)
+                    graph.mergeNodeAttributes(n.id, {
+                        x: node.x,
+                        y: node.y,
+                    })
+                })
+            })
+            .catch(console.error)
+    }
+
     let innerWidth = 0
     let innerHeight = 0
-
     let container: HTMLElement
 
     onMount(async () => {
-        type NodeProps = Omit<Data['nodes'][number], 'type'> & {
-            x: number
-            y: number
-            type: any
-            _type: Data['nodes'][number]['type']
-        }
-        type EdgeProps = Omit<Data['edges'][number], 'type'> & {
-            type: any
-            _type: Data['edges'][number]['type']
-        }
-
-        const graph = new Graph<NodeProps, EdgeProps>()
-        data.nodes.forEach((node) =>
-            graph.addNode(node.id, {
-                ...node,
-                _type: node.type,
-                x: Math.random() * innerWidth,
-                y: Math.random() * innerHeight,
-                type: undefined,
-            })
-        )
-        data.edges.forEach((edge) =>
-            graph.addEdge(edge.from, edge.to, {
-                ...edge,
-                _type: edge.type,
-                type: 'arrow',
-            })
-        )
         const renderer = new Sigma(graph, container, {
-            minCameraRatio: 0.1,
-            maxCameraRatio: 10,
+            minCameraRatio: 0.01,
+            maxCameraRatio: 1.5,
+            labelSize: 12,
+            labelColor: {
+                color: '#ffffff',
+            },
         })
+        doLayout(
+            data.nodes.map(({ id, name }) => ({ id, name })),
+            data.edges.map(({ from, to }) => ({ from, to }))
+        )
     })
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-<div bind:this={container} class="container" />
+<div bind:this={container} class="container"></div>
 
 <style>
     .container {
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,22 +1,38 @@
-import dataRaw from './data.json';
+import dataRaw from './data.json'
 
 export type Data = {
-	nodes: {
-		id: string;
-		name: string;
-		type: 'quest' | 'achievement';
-		description: string;
-		requirements: string[];
-	}[];
-	edges: {
-		to: string;
-		from: string;
-		type: 'FINISHED' | 'NOT_FINISHED' | 'IN_PROGRESS' | 'FINISHED_N_TIMES' | 'AVAILABLE';
-	}[];
-	categories: {
-		id: number;
-		name: string;
-	}[];
-};
+    nodes: {
+        id: string
+        name: string
+        type: 'quest' | 'achievement'
+        description: string
+        requirements: string[]
+    }[]
+    edges: {
+        to: string
+        from: string
+        type:
+            | 'FINISHED'
+            | 'NOT_FINISHED'
+            | 'IN_PROGRESS'
+            | 'FINISHED_N_TIMES'
+            | 'AVAILABLE'
+    }[]
+    categories: {
+        id: number
+        name: string
+    }[]
+}
 
-export const data = dataRaw as Data;
+export const data = dataRaw as Data
+export const nodeColors: { [k in Data['nodes'][number]['type']]: string } = {
+    quest: '#2B61FE',
+    achievement: '#FEDF3C',
+}
+export const edgeColors: { [k in Data['edges'][number]['type']]: string } = {
+    AVAILABLE: '#FF0000',
+    FINISHED: '#0b351050',
+    NOT_FINISHED: '#c0363650',
+    FINISHED_N_TIMES: '#36c09150',
+    IN_PROGRESS: '#e1dd3150',
+}

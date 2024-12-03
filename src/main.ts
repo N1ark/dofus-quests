0a62035d933768cda94690731626255e3ccaa26b
N1ark <n1ark.officiel@gmail.com>
2024-12-03 11:38:50 +0000

    Fix analytics insights

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { mount } from 'svelte'
 import App from './App.svelte'
 import './style.css'
 
-// // import { injectAnalytics } from '@vercel/analytics/sveltekit'
-
-// injectAnalytics({ mode: import.meta.env.DEV ? 'development' : 'production' })
-
 const app = mount(App, {
     target: document.getElementById('app')!,
 })

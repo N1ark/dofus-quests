4f39f00d9442d1747364d075d438fa856befc054
N1ark <n1ark.officiel@gmail.com>
2024-12-03 11:27:48 +0000

    Add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { mount } from "svelte";
-import App from "./App.svelte";
-import "./style.css";
+import { mount } from 'svelte'
+import App from './App.svelte'
+import './style.css'
+
+import { injectAnalytics } from '@vercel/analytics/sveltekit'
+
+injectAnalytics({ mode: import.meta.env.DEV ? 'development' : 'production' })
 
 const app = mount(App, {
-  target: document.getElementById("app")!,
-});
+    target: document.getElementById('app')!,
+})
 
-export default app;
+export default app

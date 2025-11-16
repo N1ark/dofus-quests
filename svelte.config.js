5c82bce5f336c500bb6c9c325dbb4e43e167dc77
N1ark <opale.sjostedt@gmail.com>
2025-11-16 14:32:13 +0000

    Fix for CloudFlare?

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,7 @@
-import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
     preprocess: vitePreprocess(),
-    kit: {
-        adapter: adapter({
-            // See below for an explanation of these options
-            config: undefined,
-            platformProxy: {
-                configPath: undefined,
-                environment: undefined,
-                persist: undefined,
-            },
-            fallback: 'plaintext',
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>'],
-            },
-        }),
-    },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { cloudflare } from '@cloudflare/vite-plugin'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [svelte(), cloudflare()],
 })
